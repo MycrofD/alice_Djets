9ad3293d7ff19d69b617a2c815119aaf2b84d237
Barbara Trzeciak <barbara.antonina.trzeciak@cern.ch>
2018-09-11 11:10:10 +0200

    normalization counter - combitibility with previous method w/o norm counter

--- Djets/FDsubtraction/subtractFD.C
+++ Djets/FDsubtraction/subtractFD.C
@@ -25,7 +25,8 @@ TString comMatrixFile = "",
 TString outSpectraDir = "out_FDPythia",
 bool isDptcut = 1,
 bool fold = 1, TString outHistName = "ptSpectrumSim_",
-bool isSys = 1, bool rebinned = 1,  bool isEff = 1 )
+bool isSys = 1, bool rebinned = 1,  bool isEff = 1,
+bool oldCounter = 1 )
 {
 
     gSystem->Load(Form("%s",roounfoldpwd.Data()));
@@ -38,19 +39,24 @@ bool isSys = 1, bool rebinned = 1,  bool isEff = 1 )
     gSystem->Exec(Form("mkdir %s",outPlotDir.Data()));
 
     TFile *File = new TFile(dataAnalysisFile,"read");
-    TDirectoryFile* dir=(TDirectoryFile*)File->Get("PWG3_D2H_DmesonsForJetCorrelationsMBN0");
-    AliNormalizationCounter *c = (AliNormalizationCounter*)dir->Get("NormalizationCounter");
-       //nEventsForNorm = nEventsForNorm+c->GetNEventsForNorm();
-    double nEv = c->GetNEventsForNorm();
-
-    /*TDirectoryFile* dir=(TDirectoryFile*)File->Get("DmesonsForJetCorrelations");
+    TDirectoryFile* dir;
     TList *histList;
-    if(fDmesonSpecie) histList =  (TList*)dir->Get("histosDStarMBN0");
-    else histList =  (TList*)dir->Get("histosD0MBN0");
-    TH1F* hEvents = (TH1F*)histList->FindObject("hstat");
-    //double nEvSel = hEvents->GetBinContent(2);
-    //double nEvAna = hEvents->GetBinContent(1);
-    //double nEv = nEvScale*nEvSel;*/
+    double nEv;
+    
+    if(oldCounter) {
+      dir = (TDirectoryFile*)File->Get("DmesonsForJetCorrelations");
+      if(fDmesonSpecie) histList = (TList*)dir->Get("histosDStarMBN0");
+      else histList = (TList*)dir->Get("histosD0MBN0");
+      TH1F* hEvents = (TH1F*)histList->FindObject("hstat");
+      double nEvSel = hEvents->GetBinContent(2);
+      double nEvAna = hEvents->GetBinContent(1);
+      nEv = nEvScale*nEvSel;
+    }
+    else {
+      dir = (TDirectoryFile*)File->Get("PWG3_D2H_DmesonsForJetCorrelationsMBN0");
+      AliNormalizationCounter *c = (AliNormalizationCounter*)dir->Get("NormalizationCounter");
+      nEv = c->GetNEventsForNorm();
+    }
 
     double dataLum = nEv/(sigma_in*1000) ;//Luminosity in mbar
     double simScaling;
