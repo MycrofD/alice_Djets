ebff5c6cb027c291ecf1b122bb53a218024397a0
Auro Mohanty <aurogreenindia@gmail.com>
2022-04-24 23:28:49 +0200

    D jets vs. inclusive charged jets

--- Djets/Preliminaryplots/Paperplots/drawDvIncJet_paper.py
+++ Djets/Preliminaryplots/Paperplots/drawDvIncJet_paper.py
@@ -7,10 +7,15 @@ import array
 import yaml
 #import numpy as np
 
+#ROOT.gStyle.SetPadRightMargin(0.03)
+#ROOT.gStyle.SetPadLeftMargin(0.19)
 ROOT.gStyle.SetOptStat(000)
 ROOT.gStyle.SetLegendFont(42)
-ROOT.gStyle.SetPadLeftMargin(0.19)
-ROOT.gStyle.SetPadRightMargin(0.03)
+#ROOT.gStyle.SetPadRightMargin(0.0011)
+#ROOT.gStyle.SetPadLeftMargin(0.195)
+##ROOT.gStyle.SetPadRightMargin(0.195)
+##ROOT.gStyle.SetPadLeftMargin(0.0011)
+ROOT.gStyle.SetPadBottomMargin(0.15)
 
 os.system('cp ../../finalSpectra/5TeVunc.yaml 5TeVunc.yaml')
 
@@ -18,26 +23,34 @@ os.system('cp ../../finalSpectra/5TeVunc.yaml 5TeVunc.yaml')
 if len(sys.argv)!=3:
     print("python file.py R(2,3,4,6) sys.yaml")
 ### SETTINGS
-flag_paper = 0 # turn off this flag if you want to get all four R together a, b, c, d
 Rpar=int(sys.argv[1]) # R=2,3,4,6
+if Rpar == 2:
+    ROOT.gStyle.SetPadLeftMargin(0.195)
+    ROOT.gStyle.SetPadRightMargin(0.0011)
+elif Rpar == 3:
+    ROOT.gStyle.SetPadLeftMargin(0.195)
+    ROOT.gStyle.SetPadRightMargin(0.011)
+else:
+    ROOT.gStyle.SetPadLeftMargin(0.0011)
+    ROOT.gStyle.SetPadRightMargin(0.195)
 incIndex = {2:1,3:2,4:3,6:4} # index for radius of inclusive jets
-enumIndex= {2:"(a)",3:"(b)",4:"(c)",6:"(d)"}
-if flag_paper:
-    enumIndex = {2:"(a)",3:"",4:"(b)",6:"(c)"}
+enumIndex = {2:"(a)",3:"",4:"(b)",6:"(c)"}
 energy = "5.02"
-plotmin, plotmax = 5,50
+plotmin, plotmax = 3,52
 plotYmin, plotYmax = 0,0.17;
 fptbinsJlh = [5,6,8,10,14,20,30,50]
 fptbinsJN = len(fptbinsJlh)-1
 Colors = [ROOT.kRed+1,ROOT.kBlue+2,ROOT.kGreen+2,ROOT.kViolet+2]
-Markers = [20,21,22,23,24,4]
-x_title = "#it{p}_{T,ch. jet} (GeV/#it{c})" 
-y_title = "#frac{d^{2}#it{#sigma}^{D^{0}-jets}}{d#it{p}_{T,ch. jet}d#it{#eta}_{ch. jet}} / #frac{d^{2}#it{#sigma}^{inclusive jets}}{d#it{p}_{T,ch. jet}d#it{#eta}_{ch. jet}}"
-#y_title = "#sigma(D^{0}-jets) / #sigma(inclusive jets)"
+#Markers = [20,21,22,23,24,4]
+Markers = [20,21,22,23,24,71]
+x_title = "#it{p}_{T,ch jet} (GeV/#it{c})  " 
+y_title = "#frac{d^{2}#it{#sigma}^{D^{0} jets}}{d#it{p}_{T,ch jet}d#it{#eta}_{ch jet}} #lower[0.25]{#scale[2]{/}} #frac{d^{2}#it{#sigma}^{inclusive jets}}{d#it{p}_{T,ch jet}d#it{#eta}_{ch jet}}"
+#y_title = "#sigma(D^{0} jets) / #sigma(inclusive jets)"
 #y_title = "D^{0}-jet fraction "
 #y_title = "D^{0} tagged charged particle jet fraction  "
 ## Settings others
-textsize=0.035
+textsize=0.045
+textsize1=0.05
 markersize=1.5
 ptval, value, ptvalunc, valueerrup, valueerrdown = [], [], [], [], []
 valDA, valDAerrup, valDAerrdo = [], [], []
@@ -69,7 +82,7 @@ print(djetsDo)
 fileDJ = ROOT.TFile("/eos/user/a/amohanty/home/jackbauer/Work/alice/analysis/pp5TeV/D0jet/results_APW/Final_DzeroR0"+str(Rpar)+"_paperCuts/Default/unfolding_Bayes_5/final/JetPtSpectrum_final_fullGlobal_addedCUTandJES.root","read")
 fileIJ = ROOT.TFile("/eos/user/a/amohanty/home/jackbauer/ALICE_HeavyFlavour/work/Djets/alice_Djets/InclusiveJetsPP502TeV/HEPData-ins1733689-v1-root.root","read")
 ####### Creating DATA histograms
-# D-jets
+# D jets
 hDJ = fileDJ.Get("hData_binned");
 # Inclusive jets
 dirIJ = fileIJ.Get("Jets in pp 5.02 TeV") #dirIJ = fileIJ.Get("Jets with UE subtraction in pp 5.02 TeV")
@@ -91,7 +104,7 @@ for i in range(hIJ.GetNbinsX()):
     hIJsyst.SetBinContent(i+1,hIJsyst.GetBinContent(i+1)*hIJsyst.GetBinWidth(i+1))
     hIJ.SetBinError(i+1,hIJ.GetBinError(i+1)*hIJ.GetBinWidth(i+1))
     hIJsyst.SetBinError(i+1,hIJsyst.GetBinError(i+1)*hIJsyst.GetBinWidth(i+1))
-## D-jets: setting bin error for the histos according to their bin width 
+## D jets: setting bin error for the histos according to their bin width 
 for i in range(hDJ.GetNbinsX()):
     hDJ.SetBinContent(i+1,hDJ.GetBinContent(i+1)*hDJ.GetBinWidth(i+1))
     hDJ.SetBinError(i+1,hDJ.GetBinError(i+1)*hDJ.GetBinWidth(i+1))
@@ -108,7 +121,7 @@ hDJReb.Sumw2()
 
 hR_Data = hDJReb.Clone("hR_Data")
 hR_Data.Divide(hIJReb)
-### assigning systematic uncertainties of D-jets as statistical uncertainties
+### assigning systematic uncertainties of D jets as statistical uncertainties
 hR_DatasystUp = hDJReb.Clone("hR_DataSyst")
 hR_DatasystDo = hDJReb.Clone("hR_DataSyst")
 for i in range(fptbinsJN):
@@ -138,7 +151,7 @@ hDJpow8Err = [fileDJmonash.Get("hsimPowhegPythia8_up"),fileDJmonash.Get("hsimPow
 fileIJpowheg = ROOT.TFile("/eos/user/a/amohanty/home/jackbauer/ALICE_HeavyFlavour/work/Djets/alice_Djets/InclusiveJetsPP502TeV/theoryPowhegR0"+str(Rpar)+".root")
 hIJpow8 = fileIJpowheg.Get("R0"+str(Rpar)+"PwgScaleErr")
 ###### Histograms UNSCALE AND REBIN:
-##--D-jets min bias pythia 8
+##--D jets min bias pythia 8
 for i in range(hDJmb.GetNbinsX()):
     hDJmb.SetBinContent(i+1,hDJmb.GetBinContent(i+1)*hDJmb.GetBinWidth(i+1))
     hDJmb.SetBinError(i+1,hDJmb.GetBinError(i+1)*hDJmb.GetBinWidth(i+1))
@@ -150,7 +163,7 @@ for i in range(hIJmb.GetNbinsX()):
     hIJmb.SetBinError(i+1,hIJmb.GetBinError(i+1)*hIJmb.GetBinWidth(i+1))
 
 hIJmbReb = hIJmb.Rebin(fptbinsJN,'hIJmbReb',array.array('d',fptbinsJlh))
-##--D-jets  powheg pythia 8
+##--D jets  powheg pythia 8
 for i in range(hDJpow8.GetNbinsX()):
     hDJpow8.SetBinContent(i+1,hDJpow8.GetBinContent(i+1)*hDJpow8.GetBinWidth(i+1))
     hDJpow8Err[0].SetBinContent(i+1,hDJpow8Err[0].GetBinContent(i+1)*hDJpow8Err[0].GetBinWidth(i+1))
@@ -198,8 +211,11 @@ for i in range(hR_pow8.GetNbinsX()):
 hR_pow8Err[0].SetLineColor(ROOT.kOrange+20)
 hR_pow8Err[1].SetLineColor(ROOT.kOrange+20)
 hR_pow8.SetLineColor(ROOT.kBlue+2)
-hR_mb.SetLineColor(ROOT.kOrange+2)
-hR_mb.SetMarkerColor(ROOT.kOrange+2)
+#hR_mb.SetLineColor(ROOT.kOrange+2)
+#hR_mb.SetMarkerColor(ROOT.kOrange+2)
+hR_mb.SetLineStyle(6)
+hR_mb.SetLineColor(ROOT.kOrange+7)
+hR_mb.SetMarkerColor(ROOT.kOrange+7)
 hR_mb.SetMarkerSize(0)
 hR_mb.SetLineWidth(3)
 
@@ -209,7 +225,7 @@ hR_Data.SetMarkerSize(markersize)
 hR_Data.SetMarkerStyle(Markers[0])
 hR_Data.SetMarkerColor(ROOT.kRed+2)
 
-hR_Data.GetYaxis().SetTitle(" cross section ratio for R=0."+str(Rpar)+", #frac{cross section_{D-jets}}{cross section_{inclusive jets}}")
+hR_Data.GetYaxis().SetTitle(" cross section ratio for R=0."+str(Rpar)+", #frac{cross section_{D^{0} jets}}{cross section_{inclusive jets}}")
 
 hR_DatasystUp.SetLineColor(ROOT.kBlue+2)
 hR_DatasystDo.SetLineColor(ROOT.kBlue+2)
@@ -238,16 +254,6 @@ grsys.SetMarkerStyle(Markers[5])#71)#107)
 grsys.SetMarkerColor(Colors[1])
 grsys.SetMarkerSize(markersize);
 grsys.SetTitle('')
-#grsys.GetXaxis().SetTitle(x_title);
-#grsys.GetYaxis().SetTitle(y_title);
-#grsys.GetXaxis().SetLabelSize(0.04);
-#grsys.GetXaxis().SetTitleSize(0.04);
-#grsys.GetXaxis().SetTitleOffset(1.1);
-#grsys.GetYaxis().SetTitleOffset(1.6);
-#grsys.GetYaxis().SetLabelSize(0.04);
-#grsys.GetYaxis().SetTitleSize(0.04);
-#grsys.GetXaxis().SetRangeUser(plotmin,plotmax);
-#grsys.GetYaxis().SetRangeUser(plotYmin,plotYmax);
 ### DATA SYS
 grdat = ROOT.TGraphAsymmErrors(fptbinsJN,array.array('d',ptval),array.array('d',valDA),array.array('d',ptvalunc),array.array('d',ptvalunc),array.array('d',valDAerrdo),array.array('d',valDAerrup))
 grdat.SetFillColor(ROOT.TColor.GetColor("#cccccc"))
@@ -260,50 +266,80 @@ grdat.SetMarkerSize(markersize)
 grdat.SetTitle('')
 grdat.GetXaxis().SetTitle(x_title);
 grdat.GetYaxis().SetTitle(y_title);
-grdat.GetXaxis().SetLabelSize(0.04);
-grdat.GetXaxis().SetTitleSize(0.04);
-grdat.GetXaxis().SetTitleOffset(1.1);
-grdat.GetYaxis().SetTitleOffset(1.9);
-grdat.GetYaxis().SetLabelSize(0.04);
-grdat.GetYaxis().SetTitleSize(0.04);
+grdat.GetXaxis().SetLabelSize(textsize);
+grdat.GetYaxis().SetLabelSize(textsize);
+grdat.GetXaxis().SetTitleSize(textsize1);
+grdat.GetYaxis().SetTitleSize(textsize1);
+grdat.GetXaxis().SetTitleOffset(1.2);
+grdat.GetYaxis().SetTitleOffset(1.7);
+### if flag_paper
+#if Rpar == "2":
+#    grdat.GetYaxis().SetLabelSize(0);
+#    grdat.GetYaxis().SetTitleSize(0);
+#else:
+#    grdat.GetYaxis().SetLabelSize(textsize);
+#    grdat.GetYaxis().SetTitleSize(textsize);
 grdat.GetXaxis().SetRangeUser(plotmin,plotmax);
-grdat.GetYaxis().SetRangeUser(plotYmin,plotYmax);
+if Rpar == 3:
+    grdat.GetYaxis().SetRangeUser(plotYmin,0.21);
+else:
+    grdat.GetYaxis().SetRangeUser(plotYmin,plotYmax);
 
 ############################## FORMAL STUFF
-ptL, ptR, pt_row = [], [], []
+pavetext_left, pavetext_right = [], []
+pavetext_left_R03 = []
 for i in range(4): # number of jet radii to be plotted
-    if(i==1):ptL.append(ROOT.TPaveText(0.22,0.80,0.65,0.95,"NB NDC"))
-    #elif(i==2):ptL.append(ROOT.TPaveText(0.22,0.80,0.65,0.95,"NB NDC"))
-    else:ptL.append(ROOT.TPaveText(0.22,0.87,0.65,0.95,"NB NDC"))
+    if i==0: 
+        pavetext_left.append(ROOT.TPaveText(0.22,0.87,0.65,0.95,"NB NDC"))
+    elif i==2:
+        pavetext_left.append(ROOT.TPaveText(0.05,0.79,0.55,0.95,"NB NDC"))
+    else:
+        pavetext_left.append(ROOT.TPaveText(0.05,0.87,0.55,0.95,"NB NDC"))
 
-    ptL[i].SetBorderSize(0)
-    ptL[i].SetFillStyle(0)
-    ptL[i].SetTextAlign(13)
-    ptL[i].SetTextFont(43)
-    ptL[i].SetTextSize(20)
+    pavetext_left[i].SetBorderSize(0)
+    pavetext_left[i].SetFillStyle(0)
+    pavetext_left[i].SetTextAlign(13)
+    pavetext_left[i].SetTextFont(43)
+    pavetext_left[i].SetTextSize(25)
 
-    ptR.append(ROOT.TPaveText(0.75,0.87,0.85,0.95,"NB NDC"))
-    ptR[i].SetBorderSize(0)
-    ptR[i].SetFillStyle(0)
-    ptR[i].SetTextAlign(13)
-    ptR[i].SetTextFont(43)
-    ptR[i].SetTextSize(20)
+    if i==0: 
+        pavetext_right.append(ROOT.TPaveText(0.75,0.87,0.85,0.95,"NB NDC"))
+    elif i==1: 
+        pavetext_right.append(ROOT.TPaveText(0.8,0.87,0.9,0.95,"NB NDC"))
+    else:
+        pavetext_right.append(ROOT.TPaveText(0.55,0.87,0.65,0.95,"NB NDC"))
+
+    pavetext_right[i].SetBorderSize(0)
+    pavetext_right[i].SetFillStyle(0)
+    pavetext_right[i].SetTextAlign(13)
+    pavetext_right[i].SetTextFont(43)
+    pavetext_right[i].SetTextSize(25)
     
-    ptR[i].AddText(enumIndex[Rpar]+" #it{R} = 0.%d"%(int(Rpar)))
+    pavetext_right[i].AddText(enumIndex[Rpar]+" #it{R} = 0.%d"%(int(Rpar)))
 
-ptL[0].AddText("ALICE, pp, #sqrt{#it{s}} = 5.02 TeV")
-if flag_paper:
-    ptL[2].AddText("pp, #sqrt{#it{s}} = 5.02 TeV")
-    ptL[2].AddText("Charged jets, Anti-#it{k}_{T}, |#it{#eta}_{ch. jet}| < 0.9 - #it{R}")
-    ptL[2].AddText("2 < #it{p}_{T,D^{0}} < 36 GeV/#it{c}") 
-else:
-    ptL[1].AddText("pp, #sqrt{#it{s}} = 5.02 TeV")
-    ptL[1].AddText("Charged jets, Anti-#it{k}_{T}, |#it{#eta}_{ch. jet}| < 0.9 - #it{R}")
-    ptL[2].AddText("2 < #it{p}_{T,D^{0}} < 36 GeV/#it{c}") 
+pavetext_left[0].AddText("ALICE, pp, #sqrt{#it{s}} = 5.02 TeV")
+pavetext_left[2].AddText("charged jets, anti-#scale[0.5]{ }#it{k}_{T}")
+pavetext_left[2].AddText("|#it{#eta}_{ch jet}| < 0.9 #font[122]{-}#it{R}")
+pavetext_left[3].AddText("2 < #it{p}_{T,D^{0}} < 36 GeV/#it{c}") 
 
-
+if Rpar == 3:
+    pavetext_left_R03.append(ROOT.TPaveText(0.22,0.88,0.65,0.95,"NB NDC"))
+    pavetext_left_R03.append(ROOT.TPaveText(0.22,0.77,0.65,0.88,"NB NDC"))
+    pavetext_left_R03[0].AddText("ALICE, pp, #sqrt{#it{s}} = 5.02 TeV")
+    pavetext_left_R03[1].AddText("charged jets, anti-#scale[0.5]{ }#it{k}_{T}, |#it{#eta}_{ch jet}| < 0.6")
+    pavetext_left_R03[1].AddText("2 < #it{p}_{T,D^{0}} < 36 GeV/#it{c}") 
+    pavetext_left_R03[0].SetTextSize(25)
+    pavetext_left_R03[1].SetTextSize(22)
+    for i in range(2):
+        pavetext_left_R03[i].SetBorderSize(0)
+        pavetext_left_R03[i].SetFillStyle(0)
+        pavetext_left_R03[i].SetTextAlign(13)
+        pavetext_left_R03[i].SetTextFont(43)
 ############################## CANVAS
 leg = ROOT.TLegend(0.22,0.7,0.65,0.85);
+if Rpar == 3:
+    leg = ROOT.TLegend(0.22,0.55,0.65,0.7);
+
 leg.SetTextSize(textsize);
 leg.SetFillColor(0);
 leg.SetFillStyle(0);
@@ -319,13 +355,19 @@ leg.AddEntry(grdat,"Data","pf");
 leg.AddEntry(hR_mb,"PYTHIA 8 HardQCD Monash 2013","l");
 leg.AddEntry(grsys,"POWHEG hvq + PYTHIA 8","pf");
 if(Rpar==2):leg.Draw("same");
-ptL[incIndex[Rpar]-1].Draw()
-ptR[incIndex[Rpar]-1].Draw()
+if(Rpar==3):
+    pavetext_left_R03[0].Draw("same")
+    pavetext_left_R03[1].Draw("same")
+    leg.Draw("same")
+    pavetext_right[incIndex[Rpar]-1].Draw()
+else:
+    pavetext_left[incIndex[Rpar]-1].Draw()
+    pavetext_right[incIndex[Rpar]-1].Draw()
 
 cDvI.Update()
 
-cDvI.SaveAs("plots/DJetsVIncJets_R0"+str(Rpar)+".png")
-cDvI.SaveAs("plots/DJetsVIncJets_R0"+str(Rpar)+".pdf")
+cDvI.SaveAs("plots/DJetsVIncJets_R0"+str(Rpar)+"_paper.png")
+cDvI.SaveAs("plots/DJetsVIncJets_R0"+str(Rpar)+"_paper.pdf")
 ofile = ROOT.TFile("DvInc_R0"+str(Rpar)+".root","recreate")
 hR_Data.Write("hR_Data_R0"+str(Rpar))
 ofile.Close()
